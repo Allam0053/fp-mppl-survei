cd46d045e54b3986c1ef77be23bfbc8a4104d467
Iwan Dwi Prakoso <iwan.dprakoso@gmail.com>
2021-12-01 19:41:30 +0700

    feat: add surveys model, migrations

---
+++ database/migrations/2021_12_01_123149_create_responses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResponsesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("survey_id");
+            $table->unsignedBigInteger("customer_id");
+            $table->unsignedTinyInteger("response");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('responses');
+    }
+}
