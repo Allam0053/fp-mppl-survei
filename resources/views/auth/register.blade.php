c5e5513e568939f892d7cb2a790d12777a9ec63b
Allam Taju Sarof <55141556+Allam0053@users.noreply.github.com>
2021-12-04 16:02:35 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,27 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layouts.auth')
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+@section('content')
+<form role="form" action="{{ route('register') }}">
+    @csrf
 
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
-
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+    <label>Name</label>
+    <div class="mb-3">
+        <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="name-addon" name="name">
+    </div>
+    <label>Email</label>
+    <div class="mb-3">
+        <input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="email-addon" name="email">
+    </div>
+    <label>Password</label>
+    <div class="mb-3">
+        <input type="email" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon" name="password">
+    </div>
+    <div class="form-check form-switch">
+        <input class="form-check-input" type="checkbox" id="rememberMe" checked="">
+        <label class="form-check-label" for="rememberMe">Remember me</label>
+    </div>
+    <div class="text-center">
+        <button type="button" class="btn bg-gradient-info w-100 mt-4 mb-0">Sign in</button>
+    </div>
+</form>
+@endsection
