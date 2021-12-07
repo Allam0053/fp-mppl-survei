093fa72d27363d20f9a7d38475a388f29363c5a0
Allam Taju Sarof <55141556+Allam0053@users.noreply.github.com>
2021-12-07 16:11:50 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('survey-data', [SurveyController::class, 'getSurveyDataPage'])->name('survey-data');
-Route::get('survey-data/add-question', [SurveyController::class, 'getSurveyDataAddForm'])->name('survey-data-form');
-Route::post('survey-data', [SurveyController::class, 'postSurveyData'])->name('survey-data-post');
-Route::get('survey-data/{id}/edit-question', [SurveyController::class, 'getSurveyDataEditForm'])->name('survey-data-form-edit');
-Route::put('survey-data/{id}', [SurveyController::class, 'putSurveyData'])->name('survey-data-put');
+Route::middleware('auth')->group(function () {
+    Route::get('survey-data', [SurveyController::class, 'getSurveyDataPage'])->name('survey-data');
+    Route::get('survey-data/add-question', [SurveyController::class, 'getSurveyDataAddForm'])->name('survey-data-form');
+    Route::post('survey-data', [SurveyController::class, 'postSurveyData'])->name('survey-data-post');
+    Route::get('survey-data/{id}/edit-question', [SurveyController::class, 'getSurveyDataEditForm'])->name('survey-data-form-edit');
+    Route::put('survey-data/{id}', [SurveyController::class, 'putSurveyData'])->name('survey-data-put');
 
-Route::get('survey-data/result', [SurveyController::class, 'getSurveyResultPage'])->name('survey-data-result');
+    Route::get('survey-data/result', [SurveyController::class, 'getSurveyResultPage'])->name('survey-data-result');
+});
 
 Route::get('isi-survey', [SurveyController::class, 'getSurveyPage'])->name('isi-survey');
 Route::post('isi-survey', [SurveyController::class, 'postSurveyResponses'])->name('isi-survey-post');
